c7fdf0b31e9d71e290fe3f0b411afd5159e5a0b3
Armen Safieh-Garabedian <armen.safieh-garabedian@tdsecurities.com>
2016-06-10 16:02:58 +0100

    second commit

---
+++ angular/scripts/controllers/home.js
@@ -0,0 +1,15 @@
+// code style: https://github.com/johnpapa/angular-styleguide 
+
+(function() {
+    'use strict';
+    angular
+      .module('app')
+      .controller('HomeCtrl', Chart);
+
+      function Chart($scope){
+        var vm = $scope;
+
+
+      }
+      
+})();
